4c274ac1b0de671ec26dc5f5a20b5158e17bc4d5
张海军 <739663407@qq.com>
2017-11-29 15:46:19 +0800

    动画加载效果

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode, ViewEncapsulation} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+
+
+import './app/core/preloader/preloader';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  defaultEncapsulation: ViewEncapsulation.Emulated,
+  preserveWhitespaces: false
+}).then(() => {
+  if ((<any>window).appBootstrap) {
+    (<any>window).appBootstrap();
+  }
+}).catch(err => console.log(err))
